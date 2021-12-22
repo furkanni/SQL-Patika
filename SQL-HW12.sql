a8a9f52a3f741257653d972962dbf2c8d28afefb
furkanni <92591324+furkanni@users.noreply.github.com>
2021-12-23 00:23:06 +0300

    Add files via upload

---
+++ SQL-HW12.sql
@@ -0,0 +1,22 @@
+-- 1) film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+SELECT COUNT (title) FROM film 
+WHERE length > (
+SELECT AVG (length) FROM film
+);
+-- 2) film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+SELECT COUNT (title) FROM film 
+WHERE rental_rate = (
+SELECT MAX(rental_rate) FROM film); 
+-- 3) film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+(SELECT title FROM film 
+WHERE rental_rate = (
+SELECT MIN(rental_rate) FROM film))
+UNION 
+(SELECT title FROM film 
+WHERE replacement_cost  = (
+SELECT MIN(replacement_cost) FROM film))
+-- 4) payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+SELECT customer.first_name, customer.last_name, payment.amount FROM customer
+JOIN payment ON customer.customer_id = payment.customer_id
+WHERE payment.amount = (SELECT MAX (amount) FROM payment)
+ORDER BY first_name; 
